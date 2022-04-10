22de0993e4ee7f9a684ce2e9aad6e37c9c36e2c9
goncalves2500 <103079267+goncalves2500@users.noreply.github.com>
2022-04-10 11:51:48 -0300

    Update cadastra_album_v1.py

--- cadastra_album_v1.py
+++ cadastra_album_v1.py
@@ -1,51 +1,49 @@
 from tkinter import *
 
-def registro():
+def cadastro():
     nome1 = nome_album.get()
     ano1 = ano.get()
     criador1 = nome_criador.get()
     lancamento1 = lancamento.get()
 
     if nome1 == "" or ano1 == "" or criador1 == "" or lancamento1 == "":
-        mensagem.set("Preencha o(s) espaço(s) em branco")
-    elif len(ano1) < 4 or len(ano1) > 4:
+        mensagem.set("Preencha o(s) espaço(s) em branco")#set usado para não criar uma label para cada mensagem
+    elif len(ano1) != 4:
           mensagem.set("Ano inválido!")
     else:
+        #condicional para responder "álbum de lançamento?"
         if lancamento1 == 1:    
-            arqv = open("album.txt", "a", encoding="utf-8")
-            arqv.write(f"Sim, {nome1}, {criador1}, {ano1} \n")
+            with open("album.txt", "a") as arqv:#with para não ter que fechar explicitamente o arquivo
+                arqv.write(f"Sim, {nome1}, {criador1}, {ano1} \n")
         else:
-            arqv = open("album.txt", "a", encoding="utf-8")
-            arqv.write(f"Não, {nome1}, {criador1}, {ano1} \n")
+            with open("album.txt", "a") as arqv:
+                arqv.write(f"Não, {nome1}, {criador1}, {ano1} \n")
         mensagem.set("Dados cadastrados com sucesso!")
 
-        
-global root
 root = Tk()
 root.title("Cadastrar Álbum")
 root.geometry("400x400")
 
-global nome_album, ano, nome_criador, lancamento, mensagem
 nome_album = StringVar()
 ano = StringVar()
 nome_criador = StringVar()
-lancamento = IntVar()
+lancamento = IntVar()#variável inteira porque os valores dos radiobuttons são 1 e 0
 mensagem = StringVar()
 
 Label(root, text="Nome do álbum: ", font=("Calibri")).place(x=10, y=40)
-Entry(root, textvariable=nome_album).place(x=170, y=42)
+Entry(root, textvariable=nome_album).place(x=185, y=42)
 
-Label(root, text="Criador do Álbum: ", font=("Calibri")).place(x=10, y=80)
-Entry(root, textvariable=nome_criador).place(x=170, y=82)
+Label(root, text="Criador do álbum: ", font=("Calibri")).place(x=10, y=80)
+Entry(root, textvariable=nome_criador).place(x=185, y=82)
 
-Label(root, text="Ano de Lançamento: ", font=("Calibri")).place(x=10, y=120)
-Entry(root, textvariable=ano).place(x=170, y=122)
+Label(root, text="Ano de lançamento: ", font=("Calibri")).place(x=10, y=120)
+Entry(root, textvariable=ano).place(x=185, y=122)
 
 Label(root, text="Álbum de lançamento?", font=("Calibri",)).place(x=10, y=160)
-Radiobutton(root, text="Sim", variable=lancamento, value=1).place(x=170,y=160)
-Radiobutton(root, text="Não", variable=lancamento, value=0).place(x=230, y=160)
+Radiobutton(root, text="Sim", variable=lancamento, value=1).place(x=200,y=160)
+Radiobutton(root, text="Não", variable=lancamento, value=0).place(x=260, y=160)
 
-Button(root, width=10, bg="blue", fg="white", text="Cadastar álbum", font=("Calibri", 11), command=registro).place(x=140,y=240)
+Button(root, width=10, bg="blue", fg="white", text="Cadastar álbum", font=("Calibri", 11), command=cadastro).place(x=140,y=240)
 
 Button(root, width=10, bg="red", fg="white", text="Sair", command=root.quit, font=("Calibri", 11)).place(x=140,y=280)
 
