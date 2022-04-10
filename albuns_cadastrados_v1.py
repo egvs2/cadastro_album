276326ecadfeca524297d6bc1eb429cc6fdd4ce0
goncalves2500 <103079267+goncalves2500@users.noreply.github.com>
2022-04-10 11:51:24 -0300

    Update albuns_cadastrados_v1.py

--- albuns_cadastrados_v1.py
+++ albuns_cadastrados_v1.py
@@ -5,15 +5,15 @@ root = Tk()
 root.title("Álbuns Cadastrados")
 root.geometry("1000x250")
 
-arqv = open("album.txt", "r")
+with open("album.txt") as arqv:
+    lista = [linha.split(',') for linha in arqv]
 
-lista = [linha.split(',') for linha in arqv.readlines()]
-
+#widget treeview para alocar uma lista de itens
 arvore = ttk.Treeview(root, columns=("lancamento", "album", "autor", "ano"), show="headings")
-arvore.column("lancamento", minwidth=0, width=100)
-arvore.column("album", minwidth=0, width=270)
-arvore.column("autor", minwidth=0, width=270)
-arvore.column("ano", minwidth=0, width=200)
+arvore.column("lancamento", width=100)
+arvore.column("album", width=270)
+arvore.column("autor", width=270)
+arvore.column("ano", width=200)
 arvore.column("lancamento", width=220)
 arvore.heading("lancamento", text="PRIMEIRO LANÇAMENTO?")
 arvore.heading("album", text="NOME DO ÁLBUM")
@@ -21,7 +21,7 @@ arvore.heading("autor", text="AUTOR DO ÁLBUM")
 arvore.heading("ano", text="ANO DE LANÇAMENTO")
 arvore.pack()
 
-for a,b,c,d in lista:
+for a,b,c,d in lista:#adiciona item à arvore a cada lançamento, álbum, autor e ano da lista
     arvore.insert("", "end", values=(a,b,c,d))
 
 root.mainloop()
