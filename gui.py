bc11a3824a49b2c3411fce29d77a8a725e4cae91
goncalves2500 <103079267+goncalves2500@users.noreply.github.com>
2022-04-13 00:02:16 -0300

    Update and rename interface.py to gui.py

--- gui.py
+++ gui.py
@@ -1,10 +1,9 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter.ttk import Combobox
-import operacoes as ops
+import domain
 
 def tela_cadastro():
-    global root_cadastro, nome_album, ano, nome_criador, lancamento, mensagem
     root_cadastro = Tk()
     root_cadastro.title("Cadastrar Álbum")
     root_cadastro.geometry("400x400")
@@ -29,7 +28,7 @@ def tela_cadastro():
     Radiobutton(root_cadastro, text="Não", variable=lancamento, value=0).place(x=230, y=160)
 
     Button(root_cadastro, width=10, bg="blue", fg="white", text="Cadastar álbum", font=("Calibri", 11),
-    command=lambda: ops.registro(nome_album, ano, nome_criador, lancamento, mensagem)).place(x=140, y=240)   
+    command=lambda:domain.registro(nome_album, ano, nome_criador, lancamento, mensagem)).place(x=140, y=240)   
         
     Button(root_cadastro, width=10, bg="red", fg="white", text="Sair", command=root_cadastro.destroy, font=("Calibri", 11)).place(x=140,  y=280)
                                                                                                             
@@ -38,13 +37,14 @@ def tela_cadastro():
     root_cadastro.mainloop()
 
 
+
 def tela_albuns():
     global root_albuns
     root_albuns = Tk()
     root_albuns.title("Álbuns Cadastrados")
     root_albuns.geometry("1000x250")
 
-    arqv = open("album.txt", "r")
+    arqv = open("album.txt", "r", encoding="utf-8")
     lista = [linha.split(',') for linha in arqv.readlines()]
 
     arvore = ttk.Treeview(root_albuns, columns=("lancamento", "album", "autor", "ano"), show="headings")
@@ -64,8 +64,12 @@ def tela_albuns():
 
     root_albuns.mainloop()
 
+def limpar():
+   for i in arvore.get_children():
+    arvore.delete(i)
+
 def tela_nome():
-    global nome_artista, lista_albuns, root_nome, mensagem
+    global root_nome
     root_nome = Tk()
     root_nome.title("Busca por Nome")
     root_nome.geometry("1000x500")
@@ -79,14 +83,12 @@ def tela_nome():
     Label(root_nome).pack()
 
     Button(root_nome, width=10, bg="blue", fg="white", text="Procurar", font=("Calibri", 11),
-    command=lambda: ops.busca_nome(nome_artista, mensagem)).pack()
+    command=lambda: domain.busca_nome(nome_artista, mensagem, root_nome, tela_nomes_presentes)).pack()
+    Button(root_nome, width=10, bg="blue", fg="white", text="Limpar", font=("Calibri", 11), command=limpar).pack()
     Button(root_nome, width=10, bg="red", fg="white", text="Sair", font=("Calibri", 11), command=root_nome.destroy).pack()
     Label(root_nome, textvariable=mensagem).pack()
 
-    root_nome.mainloop()
-
-
-def tela_nomes_presentes(l_nomes):
+    global arvore
     arvore = ttk.Treeview(root_nome, columns=("lancamento", "album", "autor", "ano"), show="headings")
     arvore.column("lancamento", width=100)
     arvore.column("album", width=270)
@@ -99,11 +101,15 @@ def tela_nomes_presentes(l_nomes):
     arvore.heading("ano", text="ANO DE LANÇAMENTO")
     arvore.pack()
 
+    root_nome.mainloop()
+
+def tela_nomes_presentes(l_nomes):
+
     for a,b,c,d in l_nomes:
         arvore.insert("", "end", values=(a,b,c,d))
 
 def tela_ano():
-    global root_ano, ttk
+    global root_ano, ttk, valor_rb, cb
     root_ano = Tk()
     root_ano.title("Busca por Ano")
     root_ano.geometry("1000x500")
@@ -130,16 +136,17 @@ def tela_ano():
     Radiobutton(root_ano, text="Anterior a",  value="ant", variable=valor_rb).pack()
     Radiobutton(root_ano, text="Igual a",  value="igual", variable=valor_rb).pack()
     Radiobutton(root_ano, text="Posterior a",  value="post", variable=valor_rb).pack()
+    Radiobutton
 
     Label(root_ano).pack()
 
     Button(root_ano, width=10, bg="blue", fg="white", text="Procurar", font=("Calibri", 11), 
-    command=lambda: ops.busca_ano(lista_albuns, valor_rb, cb)).pack()
+    command=lambda: domain.busca_ano(lista_albuns, valor_rb, cb, tela_anos_presentes)).pack()
+    Button(root_ano, width=10, bg="blue", fg="white", text="Limpar", font=("Calibri", 11), command=limpar).pack()
     Button(root_ano, width=10, bg="red", fg="white", text="Sair", font=("Calibri", 11), command=root_ano.quit).pack()
+    Label(root_ano).pack()
 
-    root_ano.mainloop()
-
-def tela_anos_presentes(l_presente):
+    global arvore
     arvore = ttk.Treeview(root_ano, columns=("lancamento", "album", "autor", "ano"), show="headings")
     arvore.column("lancamento", width=100)
     arvore.column("album", width=270)
@@ -151,6 +158,9 @@ def tela_anos_presentes(l_presente):
     arvore.heading("autor", text="AUTOR DO ÁLBUM")
     arvore.heading("ano", text="ANO DE LANÇAMENTO")
     arvore.pack()
+    root_ano.mainloop()
+
+def tela_anos_presentes(l_presente):
 
     for a,b,c,d in l_presente:
         arvore.insert("", "end", values=(a,b,c,d))
